7a4aff0d810f31b4defd490aff85e04f183ed05b
wangyonggui <wangyonggui@shenzhenpoly.com>
2020-04-26 10:43:20 +0800

    maven依赖冲突

--- netty-server/src/main/java/com/kone/nettycombat/NettyServerApplication.java
+++ netty-server/src/main/java/com/kone/nettycombat/NettyServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NettyCombatApplication {
+public class NettyServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(NettyCombatApplication.class, args);
+        SpringApplication.run(NettyServerApplication.class, args);
     }
 
 }
